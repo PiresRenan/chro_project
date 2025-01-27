ac4d46f801bf4a43618c7b3373f10bea2c951e07
Renan Pires <renan.sp.121@hotmail.com>
2025-01-27 18:50:51 -0300

    first commit

---
+++ api/api/src/test/java/com/bancode/sangue/api/BancoDeSangueApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bancode.sangue.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoDeSangueApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
